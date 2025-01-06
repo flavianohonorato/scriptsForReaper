2177421541d26b7a67f5ecea75fad45dfc935cb3
Flaviano Honorato <flaviano.honorato@gmail.com>
2025-01-06 11:14:35 -0300

    Fix remove all markers after crete regions

--- createRegionsFromMarkers.lua
+++ createRegionsFromMarkers.lua
@@ -1,26 +1,25 @@
 function CreateRegionsFromMarkers()
-    local marker_count = reaper.CountProjectMarkers(0)
+    local retval, num_markers, num_regions = reaper.CountProjectMarkers(0)
+    local total_markers = num_markers + num_regions
     local markers = {}
 
-    for i = 0, marker_count - 1 do
-      local retval, isrgn, pos, rgnend, name, markrgnindexnumber = reaper.EnumProjectMarkers(i)
-      if not isrgn then
-        table.insert(markers, {pos = pos, name = name})
-      end
+    for i = 0, total_markers - 1 do
+        local retval, isrgn, pos, rgnend, name, markrgnindexnumber = reaper.EnumProjectMarkers(i)
+        if not isrgn then
+            table.insert(markers, {pos = pos, name = name, index = markrgnindexnumber})
+        end
     end
 
     for i = 1, #markers - 1 do
-      local start_pos = markers[i].pos
-      local end_pos = markers[i + 1].pos
-      local name = markers[i].name
-      reaper.AddProjectMarker2(0, true, start_pos, end_pos, name, -1, 0)
+        local start_pos = markers[i].pos
+        local end_pos = markers[i + 1].pos
+        local name = markers[i].name
+        reaper.AddProjectMarker2(0, true, start_pos, end_pos, name, -1, 0)
     end
 
-    for i = marker_count - 1, 0, -1 do
-      local retval, isrgn, pos, rgnend, name, markrgnindexnumber = reaper.EnumProjectMarkers(i)
-      if not isrgn then
-        reaper.DeleteProjectMarker(0, markrgnindexnumber, false)
-      end
+    for i = #markers, 1, -1 do
+        local index = markers[i].index
+        reaper.DeleteProjectMarker(0, index, false)
     end
 
     reaper.Undo_EndBlock("Create regions from markers and remove markers", -1)
